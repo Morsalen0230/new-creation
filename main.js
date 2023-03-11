fe1340b535b66f3b519b2882c660ccbfbc9d533f
Morsalen0230 <82272360+Morsalen0230@users.noreply.github.com>
2023-03-11 19:17:52 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,453 @@
+function myJunction(){
+
+function myFunction(){
+  
+let x = Math.floor(Math.random() * (120 -0+1) )+0
+
+switch (x) {
+  
+  case x=0 :
+    x ='a';
+    break;
+  case x=1:
+    x ='b' ;
+    break;
+  case x = 2 :
+     x='c' ;
+     break ;
+  case x = 3 :
+     x ='d' ;
+     break ;
+  case x = 4 :
+    x = 'e'   ;
+     break ;
+  case x =  5:
+      x = 'f';
+     break ;
+  case x = 6:
+    x = 'g';
+  break;
+  
+  case x = 7:
+     x = 'h' ;
+     break ;
+  case x =  8:
+     x = 'i' ;
+     break ;
+  case x =  9:
+     x = 'j' ;
+     break ;
+  case x =  10:
+     x = 'k' ;
+     break ;
+  case x = 11 :
+     x = 'l' ;
+     break ;
+  case x = 12 :
+     x = 'm'  ;
+     break ;
+  case x = 13 :
+     x = 'n' ;
+     break ;
+  case x = 14 :
+     x = 'o' ;
+     break ;
+  case x =  15:
+     x = 'p' ;
+     break ;
+  case x =  16:
+     x = 'q' ;
+     break ;
+  case x =  17:
+     x =  'r';
+     break ;
+  case x =  18 :
+     x = 's' ;
+     break ;
+  case x =  19:
+     x = 't' ;
+     break ;
+  case x = 20 :
+     x = 'u' ;
+     break ;
+  case x =  21:
+     x = 'v' ;
+     break ;
+  case x = 22 :
+     x = 'w' ;
+     break ;
+  case x = 23 :
+     x =  'x';
+     break ;
+  case x = 24 :
+     x = 'y' ;
+     break ;
+  case x =  25:
+     x = 'z' ;
+     break ;
+     
+  case x = 26:
+     x =  'A';
+     break ;
+  case x = 27 :
+     x =  'B';
+     break ;
+  case x = 28 :
+     x =  'C';
+     break ;
+  case x =  29:
+     x = 'D' ;
+     break ;
+  case x =  30:
+     x = 'E' ;
+     break ;
+  case x =  31:
+     x = 'F' ;
+     break ;
+  case x =  32:
+     x = 'G' ;
+     break ;
+  case x = 33 :
+     x =  'H';
+     break ;
+  case x = 34 :
+     x =  'I';
+     break ;
+  case x =35  :
+     x = 'J' ;
+     break ;
+  case x = 36 :
+     x =  'K';
+     break ;
+  case x = 37 :
+     x =  'L';
+     break ;
+  case x = 38 :
+     x =  'M';
+     break ;
+  case x = 39 :
+     x =  'N';
+     break ;
+  case x = 40 :
+     x =  'O';
+     break ;
+  case x =  41:
+     x = 'P' ;
+     break ;
+  case x =  42:
+     x =  'Q';
+     break ;
+  case x = 43 :
+     x =  'R';
+     break ;
+  case x = 44  :
+     x = 'S' ;
+     break ;
+  case x =  45 :
+     x = 'T' ;
+     break ;
+  case x =  46:
+     x = 'U' ;
+     break ;
+  case x =  47:
+     x = 'V' ;
+     break ;
+  case x = 48 :
+     x =  'W';
+     break ;
+  case x = 49 :
+     x =  'X';
+     break ;
+  case x = 50 :
+     x =  'Y';
+     break ;
+  case x = 51 :
+     x = 'Z' ;
+     break ;
+  case x = 57:
+     x= '1';
+     break
+  case x= 52:
+     x =  '2';
+     break ;
+  case x = 53 :
+     x = '3' ;
+     break ;
+  case x =  54:
+     x = '4' ;
+     break ;
+  case x = 55 :
+     x = '5' ;
+     break ;
+  case x = 56 :
+     x = '6' ;
+     break ;
+   case x =  57:
+     x =  '7';
+     break ;
+  case x = 58 :
+     x =  '8';
+     break ;
+  case x = 59 :
+     x = '9' ;
+     break ;
+  case x =  60:
+     x =  '8';
+     break ;
+  case x =  61:
+     x = '2' ;
+     break ;
+  case x = 62  :
+     x = '7' ;
+     break ;
+  case x = 63 :
+     x = '0' ;
+     break ;
+  
+  case x =  64:
+     x = '8' ;
+     break ;
+  case x = 65 :
+     x = '3' ;
+     break ;
+  case x = 66 :
+     x =  '2';
+     break ;
+  case x =  67:
+     x =  '9';
+     break ;
+  case x = 68 :
+     x = '5' ;
+     break ;
+  case x = 69 :
+     x =  '0';
+     break ;
+  case x =  70:
+     x =  '6';
+     break ;
+  case x =  71:
+     x = '5' ;
+     break ;
+  case x =  72:
+     x = '4' ;
+     break ;
+  case x = 73 :
+     x =  '7';
+     break ;
+  case x = 74 :
+     x = '9' ;
+     break ;
+  case x =  75:
+     x = '2' ;
+     break ;
+  case x = 76 :
+     x = '1' ;
+     break ;
+  case x =  78:
+     x =  '7';
+     break ;
+  case x = 79 :
+     x =  '6';
+     break ;
+  case x = 80 :
+     x =  '4';
+     break ;
+  case x = 81:
+     x = '0' ;
+     break ;
+  case x =  82:
+     x =  '6';
+     break ;
+  case x = 83 :
+     x = '9' ;
+     break ;
+     case x = 84  :
+     x = '@' ;
+     break ;
+  case x = 85:
+    x = '#' ;
+  break;
+  case x = 86:
+    x = '%';
+  break;
+  case x = 87:
+    x ='&' ;
+  break;
+  case x = 88:
+    x = '*';
+  break;
+  case x = 89:
+    x = '-';
+  break;
+  case x = 90:
+    x = '+';
+  break;
+  case x = 91:
+    x = '(';
+  break;
+  case x =92:
+    x =')';
+  break;
+  case x = 93:
+    x = '!';
+  break;
+  case x = 94:
+    x = '"';
+  break;
+  case x = 95:
+    x = "'";
+  break;
+  case x = 96:
+    x = ':';
+  break;
+  case x = 97:
+    x =';' ;
+  break;
+  case x = 99:
+    x = '/';
+  break;
+  case x = 100:
+    x = '?';
+  break;
+  case x =101:
+    x = '?';
+  break;
+  case x = 102:
+    x = '~';
+  break;
+  case x = 103:
+    x = '`';
+  break;
+  case x = 104:
+    x = '|';
+  break;
+  case x = 105:
+    x = '√';
+  break;
+  case x = 106:
+    x = '÷';
+  break;
+  case x = 107:
+    x = '×';
+  break;
+  case x = 108:
+    x = '{';
+  break;
+  case x = 109:
+    x = '}';
+  break;
+  case x =110:
+    x = '{';
+  break;
+  case x =111:
+    x = '£';
+  break;
+  case x = 112:
+    x = '$';
+  break;
+  case x = 113:
+    x = '℅';
+  break;
+  case x = 114:
+    x = '^';
+  break;
+  case x =115:
+    x = '=';
+  break;
+  case x = 116:
+    x = '[';
+  break;
+  case x = 117:
+    x = ']';
+  break;
+  case x =118:
+    x = " \ " ;
+  break;
+  case x = 119:
+    x = '<';
+  break;
+  case x = 120:
+    x ='>' ;
+  break;
+  
+  
+}
+
+return  x;
+}
+
+function c1(){
+  
+ return myFunction();
+}
+
+function c2(){
+  
+ return myFunction();
+}
+function c3() {
+
+  return myFunction();
+}
+
+function c4() {
+
+  return myFunction();
+}
+
+function c5() {
+
+  return myFunction();
+}
+
+function c6() {
+
+  return myFunction();
+}
+
+function c7() {
+
+  return myFunction();
+}
+
+function c8() {
+
+  return myFunction();
+}
+
+function c9() {
+
+  return myFunction();
+}
+
+function c10() {
+
+  return myFunction();
+}
+
+function c11() {
+
+  return myFunction();
+}
+
+function c12() {
+
+  return myFunction();
+}
+
+let count =c2() + c3() +c4()+c5()+c6()+c7()+c8()+c9()+c10()+c11()+c12()+c2();
+
+let vvv= document.getElementById('demo')
+vvv.innerHTML = count
+
+return vvv;
+
+}
+
+function click(){
+
+let wr = document.getElementById('demo')
+
+return wr.innerHTML = myJunction() 
+
+}
